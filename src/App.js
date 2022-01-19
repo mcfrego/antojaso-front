155d26a3a959b344b5ee9854d8f128eaef3e6625
Néstor Velázquez Estupiñán <nestorfrego@gmail.com>
2022-01-19 17:25:29 +0000

    Basic layout with header scaffold

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { Route, Switch, Redirect } from 'react-router-dom'
 
 import { SearchView, DetailView } from './views'
+import { Layout } from './components'
 
 export default function App () {
   return (
-    <Switch>
-      <Route path='/' exact>
-        <Redirect to='/search' />
-      </Route>
-      <Route path='/search'>
-        <SearchView />
-      </Route>
-      <Route path='/place/:placeId'>
-        <DetailView />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/search' />
+        </Route>
+        <Route path='/search'>
+          <SearchView />
+        </Route>
+        <Route path='/place/:placeId'>
+          <DetailView />
+        </Route>
+      </Switch>
+    </Layout>
   )
 }
---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import Container from 'react-bootstrap/Container'
+import Stack from 'react-bootstrap/Stack'
+
+export function Layout ({ children }) {
+  return (
+    <Container className='my-4'>
+      <header>
+        <Stack direction='horizontal' gap='2' className='mb-4'>
+          <h1 className='me-auto'>El Antojaso</h1>
+          <p>fav</p>
+        </Stack>
+      </header>
+      <main>{children}</main>
+    </Container>
+  )
+}
