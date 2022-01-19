afff62866263582a9825930c4913ac43d1d64e62
Néstor Velázquez Estupiñán <nestorfrego@gmail.com>
2022-01-19 20:48:26 +0000

    Current location logic

---
+++ src/hooks/use-current-location.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+
+export function useCurrentLocation (options = {}) {
+  const [location, setLocation] = useState()
+  const [error, setError] = useState()
+
+  const handleSuccess = position => {
+    const { latitude, longitude } = position.coords
+    setLocation({ latitude, longitude })
+  }
+
+  const handleError = error => {
+    setError(error.message)
+  }
+
+  useEffect(() => {
+    const { geolocation } = navigator
+
+    if (!geolocation) {
+      setError('Geolocation is not supported')
+      return
+    }
+
+    geolocation.getCurrentPosition(handleSuccess, handleError, options)
+  }, [options])
+
+  return [location, error]
+}
