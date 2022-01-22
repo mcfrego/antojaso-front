7cd9fb60af5509e0bef7069f40d75611cec3d4f5
Néstor Velázquez Estupiñán <nestorfrego@gmail.com>
2022-01-22 23:00:49 +0000

    search input clear value

--- src/components/FormSearch.js
+++ src/components/FormSearch.js
@@ -1,21 +1,30 @@
 import Form from 'react-bootstrap/Form'
+import InputGroup from 'react-bootstrap/InputGroup'
+import Button from 'react-bootstrap/Button'
 
 export function FormSearch ({ search, onChangeSearch, isInputDisabled }) {
   const selectedSearchHandler = event => {
-    event.preventDefault()
     onChangeSearch(event.target.value)
+  }
+
+  const clearSearchHandler = () => {
+    onChangeSearch('')
   }
 
   return (
     <Form className='mb-4'>
       <Form.Group controlId='search'>
         <Form.Label>Search</Form.Label>
-        <Form.Control
-          placeholder='e.g. Hamburguer'
-          aria-labelledby='e.g. Hamburguer'
-          disabled={isInputDisabled}
-          onChange={selectedSearchHandler}
-        />
+        <InputGroup>
+          <Form.Control
+            placeholder='e.g. Hamburguer'
+            aria-labelledby='e.g. Hamburguer'
+            value={search}
+            disabled={isInputDisabled}
+            onChange={selectedSearchHandler}
+          />
+          <Button onClick={clearSearchHandler}>Clear</Button>
+        </InputGroup>
       </Form.Group>
     </Form>
   )
