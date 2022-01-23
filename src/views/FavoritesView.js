f7dabab0204ecc1b11ef1e55150dbfe49d020b75
Néstor Velázquez Estupiñán <nestorfrego@gmail.com>
2022-01-23 13:08:30 +0000

    Favorite View grid data

--- src/views/FavoritesView.js
+++ src/views/FavoritesView.js
@@ -1,15 +1,27 @@
 import { ResultListItem } from '../components'
 import { useDetailFavResults } from '../hooks'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export function FavoritesView () {
   const { data, isLoading } = useDetailFavResults()
 
   return (
-    <>
+    <Row>
+      <h2 className='mt-4'>These are your favorites places</h2>
       {isLoading && <p>Loading ...</p>}
-      {!isLoading &&
-        data &&
-        data.map((item, key) => <ResultListItem key={key} result={item} />)}
-    </>
+      {!isLoading && data && (
+        <>
+          {data.map((item, key) => (
+            <Col key={key} lg={6} className='mt-2'>
+              <ResultListItem result={item} />
+            </Col>
+          ))}
+        </>
+      )}
+      {!isLoading && !data?.length && (
+        <p>Ops! It seems you do not have any favorite yet.</p>
+      )}
+    </Row>
   )
 }
